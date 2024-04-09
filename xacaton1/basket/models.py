e30528738dcc9bb442dd105e0df49a93b81156d8
Soodhell <pleshakovroma13@gmail.com>
2024-04-09 16:15:37 +0300

    Added fields in Basket: parried, accepted, delivery_point_address, address_of_the_recipient

--- xacaton1/basket/models.py
+++ xacaton1/basket/models.py
@@ -7,3 +7,7 @@ class Basket(models.Model):
     count = models.IntegerField(verbose_name="Кол-во(штук)")
     user = models.ForeignKey(CustomUser, on_delete=models.PROTECT)
     product = models.ForeignKey(News, on_delete=models.PROTECT)
+    parried = models.BooleanField(default=False, verbose_name="Отправил")
+    accepted = models.BooleanField(default=False, verbose_name="Забрал")
+    delivery_point_address = models.CharField(verbose_name="Адрес отправителя", null=True)
+    address_of_the_recipient = models.CharField(verbose_name="Адресс получателя", null=True)
